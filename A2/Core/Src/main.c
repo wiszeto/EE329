f94a6b2c37e8fed8597f1bd6f70351cf9a4da498
Wilson Szeto <85478086+wiszeto@users.noreply.github.com>
2023-04-11 16:45:31 -0700

    main complete but not simplified

--- A2/Core/Src/main.c
+++ A2/Core/Src/main.c
@@ -1,6 +1,61 @@
 #include "main.h"
-#include "keypad.c"
+#include "keypad.h"
+
 void SystemClock_Config(void);
+
+int set_leds(uint32_t);
+
+static const int keys[4][3] = {{1, 2, 3},
+                               {4, 5, 6},
+                               {7, 8, 9},
+                               {0xE, 0, 0xF}};
+
+static    int cols[4] = {Col1, Col2, Col3};
+static    int rows[4] = {Row1, Row2, Row3, Row4};
+
+void keypad_init() {
+    /*
+     * This function, configures row, column and keypad pin
+    */
+
+    //Making column pins output mode bits(01)
+    COL_PORT->MODER &= ~(GPIO_MODER_MODE13 | GPIO_MODER_MODE14 | GPIO_MODER_MODE15);
+    COL_PORT->MODER |= (GPIO_MODER_MODE13_0 | GPIO_MODER_MODE14_0 | GPIO_MODER_MODE15_0);
+
+    //Making row pins input mode bits(00)
+    ROW_PORT->MODER &= ~(GPIO_MODER_MODE0 | GPIO_MODER_MODE1 | GPIO_MODER_MODE2 | GPIO_MODER_MODE3);
+
+    //enable pull-down resistor bits(10)
+    ROW_PORT->PUPDR &= (GPIO_PUPDR_PUPD0 | GPIO_PUPDR_PUPD1 | GPIO_PUPDR_PUPD2 | GPIO_PUPDR_PUPD3);
+    ROW_PORT->PUPDR |= (GPIO_PUPDR_PUPD0_1 | GPIO_PUPDR_PUPD1_1 | GPIO_PUPDR_PUPD2_1 | GPIO_PUPDR_PUPD3_1);
+}
+
+int keypad_read(int cols, int row){
+    COL_PORT -> BSRR = (Col1 |Col2|Col3); //set columns to high value
+
+    //check if key is pressed
+    if (ROW_PORT->IDR & (Row1|Row2|Row3|Row4)){
+
+        //turn off columns
+        COL_PORT->BRR = (Col1 |Col2|Col3);
+        return check_row_col(NUM_OF_COLS, NUM_OF_ROWS);
+    }
+    GPIOB->BRR = GPIO_PIN_10;
+    return -1;
+}
+
+//iterate through each column and check all rows
+int check_row_col(int col, int row){
+    for (int c=0; c < col; c++){
+        COL_PORT->BSRR = cols[c]; //turn on the column
+        for (int r=0; r < row; r++){ //Check if key pressed is in the row
+            if (ROW_PORT->IDR & (rows[r])) { //Check if key pressed is in the row
+               return keys[r][c];
+            }
+        }
+    }
+    return -1;
+}
 
 int main(void)
 {
@@ -21,15 +76,38 @@ int main(void)
   GPIOB->BRR = GPIO_PIN_10;
   while (1)
   {
-      int output = keypad_read(4, 3); //get key value
+      int output = keypad_read( 4, 3); //get key value
       if (output != -1){
       // count++;
-          // set_leds(output); //set leds to value
+          set_leds(output); //set leds to value
       }
-      //set_leds(0); //if key not pressed set leds to 0
+      else {set_leds(0);} //if key not pressed set leds to 0
   }
 }
 
+int set_leds(uint32_t number) {
+	  // Make pin0 output mode
+	  GPIOA->MODER &= ~(GPIO_MODER_MODE0);
+	  GPIOA->MODER |= (GPIO_MODER_MODE0_0);
+
+	  // Make pin1 output mode
+	  GPIOA->MODER &= ~(GPIO_MODER_MODE1);
+	  GPIOA->MODER |= (GPIO_MODER_MODE1_0);
+
+	  // Make pin2 output mode
+	  GPIOA->MODER &= ~(GPIO_MODER_MODE2);
+	  GPIOA->MODER |= (GPIO_MODER_MODE2_0);
+
+	  // Make pin3 output mode
+	  GPIOA->MODER &= ~(GPIO_MODER_MODE3);
+	  GPIOA->MODER |= (GPIO_MODER_MODE3_0);
+
+
+
+	  GPIOA->ODR = number; // changes all 16 port bits to count in binary
+
+
+}
 
 void SystemClock_Config(void)
 {
