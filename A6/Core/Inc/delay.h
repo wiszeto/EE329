4ac94ff7f7e1248f075e2a73fcb6c8d7e4087099
Wilson <wilsonszerto99@gmail.com>
2023-05-20 20:13:47 -0700

    A6 finish

---
+++ A6/Core/Inc/delay.h
@@ -0,0 +1,11 @@
+
+#ifndef INC_DELAY_H_
+#define INC_DELAY_H_
+
+#include "main.h"
+
+// funciton prtotypes
+void SysTick_Init(void);
+void delay_us(const uint32_t time_us);
+
+#endif /* INC_DELAY_H_ */
--- A6/Core/Src/uart.py
+++ A6/Core/Src/uart.py
@@ -1,9 +1,45 @@
+# import serial
+
+# # Open the serial port.
+# ser = serial.Serial('COM3', 57600)
+
+# # Open the log file.
+# log_file = open("serial_log.txt", "w")
+
+# try:
+#     while True:
+#         if ser.in_waiting > 0:
+#             byte = ser.read().decode('utf-8')
+#             print(byte)
+
+#             # Write the data to the log file.
+#             log_file.write(byte)
+            
+#             log_file.flush()  # Make sure it's actually written to disk.
+
+# except KeyboardInterrupt:
+#     print("Ending and closing log file.")
+
+# finally:
+#     # Close the log file when you're done.
+#     log_file.close()
 import serial
 
 # Open the serial port.
 ser = serial.Serial('COM3', 57600)
 
-while True:
-    if ser.in_waiting > 0:
-        byte = ser.read().decode('utf-8')
-        print(byte, end='')
+try:
+    # The bytes to send.
+    bytes_to_send = b"Hello, UART!"
+
+    # Send it over the UART.
+    ser.write(bytes_to_send)
+
+except Exception as e:
+    # If there's an error, print it out.
+    print("Error: ", str(e))
+
+finally:
+    # Close the serial port when you're done with it.
+    ser.close()
+
